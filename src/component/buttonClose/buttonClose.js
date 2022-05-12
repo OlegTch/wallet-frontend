d0edf7bbd4ddd6b4d0db307cb01609a7c8061791
руслан <82108672+Ruslan-Tuliabaeiev@users.noreply.github.com>
2022-05-12 16:19:47 +0300

    spinnerLoader  /  buttonClose

---
+++ src/component/buttonClose/buttonClose.js
@@ -0,0 +1,11 @@
+import style from "./buttonClose.css";
+//import PropTypes from 'prop-types'; {onClick}
+
+const ButtonClose = () => {
+    return (
+        <button className={style.button}  >
+            +
+        </button>
+    )
+}
+export default ButtonClose;

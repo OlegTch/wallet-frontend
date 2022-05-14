152884cc2d43c42e09255a7a6d85353432f184f7
Vlad <vvllaadd1233345@gmail.com>
2022-05-14 02:35:04 +0200

    feat: home-tab full mobile

---
+++ src/component/homeTab/HomeTab.jsx
@@ -0,0 +1,95 @@
+import './homeTab.scss';
+// import {} from '@redux';
+
+const HomeTab = () => {
+    return (
+        <section className="homeTab-section">
+            <div className="container">
+                <ul>
+                    <li className="homeTab-item homeTab-item--red">
+                        <p>
+                            Дата:<span>04.01.19</span>
+                        </p>
+                        <p>
+                            Тип:<span>-</span>
+                        </p>
+                        <p>
+                            Категория:<span>Разное</span>
+                        </p>
+                        <p>
+                            Комментарий:<span>Подарок жене</span>
+                        </p>
+                        <p>
+                            Сумма:<span className="homeTab-result">300.00</span>
+                        </p>
+                        <p>
+                            Баланс:<span>6 900.00</span>
+                        </p>
+                    </li>
+                    <li className="homeTab-item homeTab-item--red">
+                        <p>
+                            Дата:<span>04.01.19</span>
+                        </p>
+                        <p>
+                            Тип:<span>-</span>
+                        </p>
+                        <p>
+                            Категория:<span>Разное</span>
+                        </p>
+                        <p>
+                            Комментарий:<span>Подарок жене</span>
+                        </p>
+                        <p>
+                            Сумма:<span className="homeTab-result">300.00</span>
+                        </p>
+                        <p>
+                            Баланс:<span>6 900.00</span>
+                        </p>
+                    </li>
+                    <li className="homeTab-item homeTab-item--green">
+                        <p>
+                            Дата:<span>04.01.19</span>
+                        </p>
+                        <p>
+                            Тип:<span>-</span>
+                        </p>
+                        <p>
+                            Категория:<span>Разное</span>
+                        </p>
+                        <p>
+                            Комментарий:<span>Подарок жене</span>
+                        </p>
+                        <p>
+                            Сумма:<span className="homeTab-result">300.00</span>
+                        </p>
+                        <p>
+                            Баланс:<span>6 900.00</span>
+                        </p>
+                    </li>
+                    <li className="homeTab-item homeTab-item--red">
+                        <p>
+                            Дата:<span>04.01.19</span>
+                        </p>
+                        <p>
+                            Тип:<span>-</span>
+                        </p>
+                        <p>
+                            Категория:<span>Разное</span>
+                        </p>
+                        <p>
+                            Комментарий:<span>Подарок жене</span>
+                        </p>
+                        <p>
+                            Сумма:<span className="homeTab-result">300.00</span>
+                        </p>
+                        <p>
+                            Баланс:<span>6 900.00</span>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+};
+
+export default HomeTab;

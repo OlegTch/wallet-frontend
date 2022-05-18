2df10952174da0aab0e44498b9327acf8cac9703
IrinaShushkevych <IraOlijnyk@gmail.com>
2022-05-18 23:08:20 +0300

    error

--- src/component/homeTabList/HomeTabList.jsx
+++ src/component/homeTabList/HomeTabList.jsx
@@ -41,13 +41,16 @@ const HomeTabList = ({ operations }) => {
                                     </li>
                                     {operations.map(operation => {
                                         return (
-                                            <>
+                                            <li
+                                                key={operation._id}
+                                                className="homeTabItemTabletAndDesktop"
+                                            >
                                                 <HomeTabItemTabletAndDesktop
                                                     operation={operation}
                                                     index
-                                                    key={operation._id}
+                                                    // key={operation._id}
                                                 />
-                                            </>
+                                            </li>
                                         );
                                     })}
                                 </>

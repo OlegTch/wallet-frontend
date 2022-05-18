f37bb9d96f79a1f294d37320250429bad8e7d6c3
Vlad <vvllaadd1233345@gmail.com>
2022-05-18 23:49:26 +0200

    feat: fix

--- src/component/homeTabItemMobile/homeTabItemMobile.jsx
+++ src/component/homeTabItemMobile/homeTabItemMobile.jsx
@@ -2,60 +2,76 @@ import './homeTabItemMobile.scss';
 import moment from 'moment';
 import EllipsisText from 'react-ellipsis-text';
 
-const homeTabItemMobile = ({
-    operation: {
-        category: { name },
-        income,
-        sum,
-        balance,
-        comment,
-        datetime,
-    },
-}) => {
+const homeTabItemMobile = ({ operations }) => {
     return (
-        <li
-            className={
-                income
-                    ? 'homeTabItem homeTabItem--green'
-                    : 'homeTabItem homeTabItem--red'
-            }
-        >
-            <p>
-                Дата:
-                <span>{moment(datetime).format('DD.MM.YY')}</span>
-            </p>
-            <p>
-                Тип:<span>{income ? '+' : '-'}</span>
-            </p>
-            <p>
-                Категорія:
-                <span>
-                    <EllipsisText text={name} length={13} />
-                </span>
-            </p>
-            <p>
-                Коментар:
-                <span>
-                    <EllipsisText
-                        text={comment === null || '' ? '-' : comment}
-                        length={13}
-                    />
-                    {comment}
-                </span>
-            </p>
-            <p>
-                Сума:
-                <span className="homeTabItemResult">
-                    <EllipsisText text={String(sum)} length={13} />
-                </span>
-            </p>
-            <p>
-                Баланс:
-                <span>
-                    <EllipsisText text={String(balance)} length={13} />
-                </span>
-            </p>
-        </li>
+        <ul className="homeTabList">
+            {operations.map(
+                ({
+                    category: { name },
+                    income,
+                    sum,
+                    balance,
+                    comment,
+                    datetime,
+                }) => {
+                    console.log(comment);
+                    return (
+                        <li
+                            className={
+                                income
+                                    ? 'homeTabItem homeTabItem--green'
+                                    : 'homeTabItem homeTabItem--red'
+                            }
+                        >
+                            <p>
+                                Дата:
+                                <span>
+                                    {moment(datetime).format('DD.MM.YY')}
+                                </span>
+                            </p>
+                            <p>
+                                Тип:<span>{income ? '+' : '-'}</span>
+                            </p>
+                            <p>
+                                Категорія:
+                                <EllipsisText text={name} length={13} />
+                            </p>
+                            <p>
+                                Коментар:
+                                <span>
+                                    <EllipsisText
+                                        text={
+                                            comment === null || ''
+                                                ? '-'
+                                                : comment
+                                        }
+                                        length={13}
+                                    />
+                                </span>
+                            </p>
+                            <p>
+                                Сума:
+                                <span className="homeTabItemResult">
+                                    <EllipsisText
+                                        text={String(sum)}
+                                        length={13}
+                                    />
+                                </span>
+                            </p>
+                            <p>
+                                Баланс:
+                                <span>
+                                    <EllipsisText
+                                        text={String(balance)}
+                                        length={13}
+                                    />
+                                </span>
+                            </p>
+                        </li>
+                    );
+                },
+            )}
+        </ul>
     );
 };
 

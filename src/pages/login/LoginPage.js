1440f956cc52dcb450b3ea42d3831b166cf633b3
IrinaShushkevych <IraOlijnyk@gmail.com>
2022-05-18 16:08:41 +0300

    fix

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -5,19 +5,7 @@ import { LoginForm } from '@component/loginForm';
 // import { RegistrationPage } from '../registration/RegistrationPage';
 // import { Dashboard } from '@pages';
 
-// import { isAuth } from '@redux/user/user-selector';
-// import { useNavigate } from 'react-router-dom';
-
 const LoginPage = () => {
-    // const isAuthUser = useSelector(isAuth);
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     if (isAuthUser) {
-    //         navigate('/');
-    //     }
-    // }, [isAuthUser]);
-
     // const isLoggedIn = useSelector(isLoading);
     // console.log(isLoggedIn);
     return (

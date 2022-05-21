009e4180bd72cd7aa8bfe31cecb7dd2e40f2f615
OlegTch <81326823+OlegTch@users.noreply.github.com>
2022-05-21 16:36:57 +0300

    add cat

---
+++ src/component/cat/cat.js
@@ -0,0 +1,11 @@
+// import cat from '../../assets/img/cat.png';
+import './cat.scss';
+
+const Cat = () => {
+    return (
+        <div className="cat">
+            <p className="catText">Транзакції відсутні</p>
+        </div>
+    );
+};
+export default Cat;

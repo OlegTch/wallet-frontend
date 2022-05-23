6e5a899549bd08eda4ea485ce31486d7a3c323a2
OlegTch <81326823+OlegTch@users.noreply.github.com>
2022-05-24 01:17:42 +0300

    add local remove

--- src/component/modalTransaction/ModalTransaction.js
+++ src/component/modalTransaction/ModalTransaction.js
@@ -27,7 +27,7 @@ export const ModalTransaction = () => {
     const [comment, setComment] = useState('');
 
     const dispatch = useDispatch();
-
+    console.log('modalTypeTransaction-1', modalTypeTransaction);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     function closeModalItem() {
         console.log('close modal dispatch');
@@ -35,7 +35,7 @@ export const ModalTransaction = () => {
     }
     function closeModalClearLocal() {
         dispatch(closeModalTransaction());
-        localStorage.clear();
+        locStorageClear();
     }
     function saveLocStorage() {
         const typeLocStor = localStorage.setItem('Тип', modalTypeTransaction);
@@ -56,13 +56,20 @@ export const ModalTransaction = () => {
         );
     }
     function locStorageClear() {
-        localStorage.clear();
+        localStorage.removeItem('Тип');
+        localStorage.removeItem('категорії');
+        localStorage.removeItem('коментар');
+        localStorage.removeItem('сума');
+        localStorage.removeItem('ідКатегорія');
     }
     function locStorageUseDate() {
-        const typeLocStor = localStorage.getItem('Тип', modalTypeTransaction);
+        const typeLocStor = localStorage.getItem('Тип');
         if (typeLocStor) {
             setModalTypeTransaction(typeLocStor);
+        } else {
+            setModalTypeTransaction('income');
         }
+        console.log('modalTypeTransaction-2', modalTypeTransaction);
         const categLocStor = localStorage.getItem('категорії', category);
         if (categLocStor) {
             setCategory(categLocStor);
@@ -114,7 +121,6 @@ export const ModalTransaction = () => {
     }, [closeModalItem]);
     useEffect(() => {
         if (pushDate) {
-            console.log('pushDate');
             closeModalItem();
             locStorageClear();
         }
@@ -123,6 +129,7 @@ export const ModalTransaction = () => {
     function dateChange(e) {
         setDate(e._d);
     }
+
     function listOpen() {
         setListActive(!listActive);
     }
@@ -167,6 +174,9 @@ export const ModalTransaction = () => {
     }
 
     function switchClickHandler(e) {
+        console.log('switchClick', e.target.checked);
+        console.dir(e.target);
+
         if (e.target.checked) {
             setModalTypeTransaction('spending');
             setCategory('Виберіть категорію');
@@ -205,7 +215,7 @@ export const ModalTransaction = () => {
             toast.error(error[0].message);
             return;
         }
-        console.log('modal dispatch');
+
         dispatch(
             getFinanceOpertaion.addOperation({
                 datetime: date,
